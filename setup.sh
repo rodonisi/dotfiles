eae39216a363ef1b6a4f20d504128c9d5ec709c8
rodonisi <simon.rodoni@gmail.com>
2025-09-01 20:30:09 +0200

    add ghostty config

--- setup.sh
+++ setup.sh
@@ -5,7 +5,10 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 NVIM_DIR=$HOME/.config/nvim
 NVIM_CONFIG="lazyvim"
 
-DEPS=["fd" "rg" "lazygit" "neovim"]
+GHOSTTY_DIR=$HOME/.config/ghostty
+GHOSTTY_CONFIG="ghostty"
+
+DEPS=["fd" "ripgrep" "lazygit" "neovim" "ghostty"]
 
 install_homebrew() {
   if [[ "$OSTYPE" != "darwin"* ]]; then
@@ -19,8 +22,8 @@ install_homebrew() {
   fi
 }
 
-setup_neovim() {
-  echo "Setting up Neovim configuration..."
+install_dependencies() {
+  install_homebrew
 
   if [[ "$OSTYPE" == "darwin"* ]]; then
     for dep in "${DEPS[@]}"; do
@@ -30,20 +33,37 @@ setup_neovim() {
       fi
     done
   fi
-
-  if [ -d "$NVIM_DIR" ] && [ ! -L "$NVIM_DIR" ]; then
-    echo "Backing up existing Neovim configuration to $NVIM_DIR.bak"
-    mv "$NVIM_DIR" "$NVIM_DIR.bak"
-  fi
-
-  if [ -e "$NVIM_DIR" ] || [ -L "$NVIM_DIR" ]; then
-    echo "Cleaning up existing Neovim configuration"
-    rm -rf "$NVIM_DIR"
-  fi
-
-  echo "Creating symbolic link for Neovim configuration"
-  ln -s "$DIR/$NVIM_CONFIG/" "$NVIM_DIR"
 }
 
-install_homebrew
+setup_dotconfig_tool() {
+  SOURCE_DIR=$1
+  TARGET_DIR=$2
+
+  echo "Setting up $SOURCE_DIR configuration"
+
+  if [ -d "$TARGET_DIR" ] && [ ! -L "$TARGET_DIR" ]; then
+    echo "Backing up existing configuration to $TARGET_DIR.bak"
+    mv "$TARGET_DIR" "$TARGET_DIR.bak"
+  fi
+
+  if [ -e "$TARGET_DIR" ] || [ -L "$TARGET_DIR" ]; then
+    echo "Cleaning up existing configuration"
+    rm -rf "$TARGET_DIR"
+  fi
+
+  echo "Creating symbolic link for $SOURCE_DIR configuration"
+  ln -s "$DIR/$SOURCE_DIR/" "$TARGET_DIR"
+
+}
+
+setup_neovim() {
+  setup_dotconfig_tool "$NVIM_CONFIG" "$NVIM_DIR"
+}
+
+setup_ghostty() {
+  setup_dotconfig_tool "$GHOSTTY_CONFIG" "$GHOSTTY_DIR"
+}
+
+install_dependencies
 setup_neovim
+setup_ghostty
