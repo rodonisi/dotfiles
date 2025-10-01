aa8a9bfebed6eb1a0a381b9cae2ce88e2cbb2143
Simon Rodoni <simon.rodoni@sensirion.com>
2025-10-01 13:15:33 +0200

    [nvim] adjust minimap config

--- lazyvim/lua/plugins/minimap.lua
+++ lazyvim/lua/plugins/minimap.lua
@@ -1,17 +1,21 @@
 return {
   "Isrothy/neominimap.nvim",
   lazy = false, -- NOTE: NO NEED to Lazy load
-  dependencies = { "nvim-treesitter/nvim-treesitter", "lewis6991/gitsigns.nvim", "nvim-mini/mini.diff" },
+  dependencies = { "nvim-treesitter/nvim-treesitter", "lewis6991/gitsigns.nvim" },
   keys = {},
   init = function()
     -- The following options are recommended when layout == "float"
     vim.opt.wrap = false
-    vim.opt.sidescrolloff = 64 -- Set a large value
+    vim.opt.sidescrolloff = 120 -- Set a large value
 
     --- Put your configuration here
-    ---@type Neominimap.UserConfig
     vim.g.neominimap = {
       auto_enable = true,
+      x_multiplier = 3,
+      y_multiplier = 1,
+      search = {
+        enabled = false,
+      },
     }
   end,
 }
