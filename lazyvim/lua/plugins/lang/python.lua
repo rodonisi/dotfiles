1e0ca9fec582009be01024a288be0b80c3266f92
Simon Rodoni <simon.rodoni@sensirion.com>
2025-11-17 10:47:23 +0100

    [nvim] switch python to black

--- lazyvim/lua/plugins/lang/python.lua
+++ lazyvim/lua/plugins/lang/python.lua
@@ -1,5 +1,32 @@
 return {
   {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("none-ls")
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, nls.builtins.formatting.black)
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["python"] = { "black" },
+      },
+      formatters = {
+        black = {
+          prepend_args = {
+            "--line-length",
+            "120",
+            "--skip-string-normalization",
+          },
+        },
+      },
+    },
+  },
+  {
     "nvim-neotest/neotest",
     dependencies = {
       "nvim-neotest/neotest-python",
@@ -10,8 +37,8 @@ return {
           dap = {
             dap = { justMyCode = false },
             args = { "--log-level", "DEBUG" },
-            runner = "pytest",
-            python = ".venv/bin/python",
+            -- runner = "pytest",
+            -- python = ".venv/bin/python",
             pytest_discover_instances = true,
           },
         },
