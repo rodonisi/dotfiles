12c66db66988377489a7e2b0790487b8aeaf44bb
Simon Rodoni <simon.rodoni@sensirion.com>
2025-11-17 10:47:14 +0100

    [nvim] add hcl

---
+++ lazyvim/lua/plugins/lang/hcl.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "terraform", "hcl" })
+      end
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        terraformls = {},
+        tflint = {},
+      },
+    },
+  },
+}
