89a1ba69fc2162643349f383f9eb03765ced8dc2
Simon Rodoni <simon.rodoni@sensirion.com>
2026-01-15 11:42:33 +0100

    [nvim] swap to sidekick

--- lazyvim/lua/plugins/ai.lua
+++ lazyvim/lua/plugins/ai.lua
@@ -1,38 +1,54 @@
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    dependencies = {
-      "copilotlsp-nvim/copilot-lsp",
-    },
-    opts = {
-      nes = { enable = true },
-    },
-  },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   dependencies = {
+  --     "copilotlsp-nvim/copilot-lsp", -- (optional) for NES functionality
+  --   },
+  --   opts = {}
+  -- },
+  -- {
+  --   "zbirenbaum/copilot-cmp",
+  --   dependencies = {
+  --     "zbirenbaum/copilot.lua",
+  --   },
+  --   opts = {}
+  -- },
+  -- {
+  --   "saghen/blink.cmp",
+  --   optional = true,
+  --   dependencies = { "fang2hou/blink-copilot" },
+  --   opts = {
+  --     sources = {
+  --       default = { "copilot" },
+  --       providers = {
+  --         copilot = {
+  --           name = "copilot",
+  --           module = "blink-copilot",
+  --           score_offset = 100,
+  --           async = true,
+  --         },
+  --       },
+  --     },
+  --   },
+  -- }
   {
     "saghen/blink.cmp",
-    optional = true,
-    dependencies = {
-      "fang2hou/blink-copilot",
-    },
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
     opts = {
-      sources = {
-        default = { "copilot" },
-        providers = {
-          copilot = {
-            opts = {
-              max_completions = 3,
-            },
-          },
-        },
-      },
-      fuzzy = {
-        sorts = {
-          "exact",
-          -- defaults
-          "score",
-          "sort_text",
+
+      keymap = {
+        ["<Tab>"] = {
+          "snippet_forward",
+          function() -- sidekick next edit suggestion
+            return require("sidekick").nes_jump_or_apply()
+          end,
+          function() -- if you are using Neovim's native inline completions
+            return vim.lsp.inline_completion.get()
+          end,
+          "fallback",
         },
       },
     },
-  },
+  }
 }
